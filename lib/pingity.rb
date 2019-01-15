fec0054bff8d56baf568236191a12323ee8eb53d
Danny Fekete <dannyfekete@gmail.com>
2019-01-15 13:01:08 -0500

    Version 1.0.0: Reports now call on pingity.com rather than localhost:3000.

--- lib/pingity.rb
+++ lib/pingity.rb
@@ -8,7 +8,7 @@ require "pingity/report"
 module Pingity
   class Error < StandardError; end
 
-  API_URL = "http://localhost:3000/api/v1/reports"
+  API_URL = "http://pingity.com/api/v1/reports"
   API_PUBLIC_KEY = ENV['PINGITY_ID']
   API_SECRET_KEY = ENV['PINGITY_SECRET']
 
